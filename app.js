bd35ba0f44ef6072be4bcfeda1e2c9a18d00eb2f
AlbertoFreije <freicartobe@gmail.com>
2021-03-30 13:24:27 +0200

    SDI-2021-106-7.3-División de las rutas en módulos, peticiones GET y parámetros.

--- app.js
+++ app.js
@@ -5,17 +5,10 @@ let app = express();
 // Variables
 app.set('port', 8081);
 
-app.get('/usuarios', function (req,res){
-    console.log("Depurar aquí");
-    res.send('ver usuarios');
+require("./routes/rusuarios.js")(app); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app); // (app, param1, param2, etc.)
 
-});
-
-app.get('/canciones', function (req,res){
-
-    res.send('ver canciones');
-});
-
+// lanzar el servidor
 app.listen(app.get('port'), function(){
     console.log('Servidor activo');
 });
