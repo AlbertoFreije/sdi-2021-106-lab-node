58bc93eb004936016fb234f85eeb1691715a3b80
AlbertoFreije <freicartobe@gmail.com>
2021-04-05 16:19:41 +0200

    SDI-2021-106-8.2-Arquitectura para el acceso a datos.

--- app.js
+++ app.js
@@ -5,17 +5,21 @@ let bodyParser = require('body-parser');
 let swig = require('swig');
 let mongo = require('mongodb');
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
 
 // Variables
 app.set('port', 8081);
 app.set('db','mongodb://admin:sdi@tiendamusica-shard-00-00.valdk.mongodb.net:27017,tiendamusica-shard-00-01.valdk.mongodb.net:27017,tiendamusica-shard-00-02.valdk.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-3saf0h-shard-0&authSource=admin&retryWrites=true&w=majority');
 
 
-require("./routes/rusuarios.js")(app, swig); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app, swig, mongo); // (app, param1, param2, etc.)
+require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
 require("./routes/rautores.js")(app, swig); // (app, param1, param2, etc.)
 
 // lanzar el servidor
