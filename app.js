5c03b6794214ff2ddb9be34011a738c66f613ce7
AlbertoFreije <freicartobe@gmail.com>
2021-04-20 23:21:20 +0200

    SDI-2021-106-10.3-SW Cliente JQuery + AJAX.

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 //Módulos
 let express = require('express');
 let app = express();
+
+//Access-Control-Allow-Origin
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, UPDATE, PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+    // Debemos especificar todas las headers que se aceptan. Content-Type , token
+    next();
+});
 let jwt = require('jsonwebtoken');
 app.set('jwt',jwt);
 let bodyParser = require('body-parser');
